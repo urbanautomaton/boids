db1e259c5aec25d62692650ed77e189c4050bd2d
Simon Coffey <simon@urbanautomaton.com>
2017-01-17 00:27:21 +0000

    [001] highly* sophisticated** flocking*** behaviour

--- src/main.js
+++ src/main.js
@@ -1,18 +1,60 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var birds = [
-  [ 10, 10 ],
-  [ 20, 20 ],
-  [ 10, 20 ],
-  [ 30, 20 ],
-  [ 30, 30 ]
-];
+var X = 500;
+var Y = 500;
+var BIRDS = 20;
+
+var vel = [];
+var pos = [];
+
+for (var i=0; i < BIRDS; i++) {
+  vel[i] = $V([.2, .2]);
+  pos[i] = $V([X/2 + Math.random() * 50, Y/2 + Math.random() * 50]);
+}
 
 ctx.fillStyle = "green";
 
-for (var i=0; i < birds.length; i++) {
-  var bird = birds[i];
+function drawBird(i) {
+  var bird = pos[i];
 
-  ctx.fillRect(bird[0], bird[1], 5, 5)
+  ctx.fillRect(
+    bird.e(1),
+    bird.e(2),
+    5,
+    5
+  );
 }
+
+function step(timestamp) {
+  console.log("Doing a bird as it were");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (!last) { last = timestamp }
+  var delta = timestamp - last;
+
+  for (var i=0; i < BIRDS; i++) {
+    pos[i] = pos[i].add(vel[i].x(delta / 1000));
+
+    drawBird(i);
+  }
+  window.requestAnimationFrame(step);
+}
+
+var last = null;
+
+window.requestAnimationFrame(step);
+
+
+// for (every frame) {
+//   // calculate each bird's velocity (maybe?)
+  
+//   for (each bird) {
+//     new_velocity = [
+//       // some repulsion factor based on proximity
+//       // some alignment factor based on neighbour vectors
+//       // some centroid factor based on neighbour position
+//     ]
+
+//     new_position = old_position + new_velocity * tick;
+//   }
+// }
