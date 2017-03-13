5c561b41827c24d2c7e17646e8afcb18fafdcf8b
Simon Coffey <simon@urbanautomaton.com>
2017-03-13 20:08:45 +0000

    Add Babel
    Get on that sweet sweet ES6 train

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
         test: require.resolve('./vendor/sylvester'),
         use: 'exports-loader?Matrix,Vector'
       }
