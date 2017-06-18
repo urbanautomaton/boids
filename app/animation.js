9962641735252cb27c0575f2dbb8d099a43aee5b
Simon Coffey <simon@urbanautomaton.com>
2017-06-18 19:51:49 +0100

    Animation linted

--- app/animation.js
+++ app/animation.js
@@ -1,16 +1,17 @@
 function Animation(doc, win, draw) {
-  this._document = doc;
-  this._window = win;
-  this._animating = false;
-  this._animation_request_ids = [];
-  this._last = null;
-  this._draw = draw;
+  this.document = doc;
+  this.window = win;
+  this.animating = false;
+  this.animation_request_ids = [];
+  this.last = null;
+  this.draw = draw;
   this.frameRate = 0;
 
-  var obj = this;
+  const obj = this;
+
   // sort this out so it doesn't clobber animating state
-  this._document.addEventListener('visibilitychange', function() {
-    if (obj._document.visibilityState === 'hidden') {
+  this.document.addEventListener('visibilitychange', () => {
+    if (obj.document.visibilityState === 'hidden') {
       obj.pause();
     } else {
       obj.play();
@@ -18,57 +19,47 @@ function Animation(doc, win, draw) {
   });
 }
 
-Animation.prototype._getNextFrame = function() {
-  var obj = this;
+Animation.prototype.getNextFrame = function getNextFrame() {
+  const obj = this;
 
-  return this._window.requestAnimationFrame(
-    function(timestamp) { obj._step(timestamp); }
-  );
+  return this.window.requestAnimationFrame((timestamp) => { obj.step(timestamp); });
 };
 
-Animation.prototype.pause = function() {
-  if (this._animating) {
-    this._animating = false;
-    for (var i=0; i<this._animation_request_ids.length; i++) {
-      this._window.cancelAnimationFrame(this._animation_request_ids[i]);
-    }
-    this._animation_request_ids = [];
+Animation.prototype.pause = function pause() {
+  if (this.animating) {
+    this.animating = false;
+    this.animation_request_ids.forEach((id) => { this.window.cancelAnimationFrame(id); });
+    this.animation_request_ids = [];
   }
 };
 
-Animation.prototype.play = function() {
-  if (!this._animating) {
-    this._animating = true;
-    this._last = null;
-    this._getNextFrame();
+Animation.prototype.play = function play() {
+  if (!this.animating) {
+    this.animating = true;
+    this.last = null;
+    this.getNextFrame();
   }
 };
 
-Animation.prototype.toggle = function() {
-  if (this._animating) {
+Animation.prototype.toggle = function toggle() {
+  if (this.animating) {
     this.pause();
   } else {
     this.play();
   }
 };
 
-Animation.prototype.animating = function() {
-  return this._animating;
-};
+Animation.prototype.step = function step(timestamp) {
+  this.animation_request_ids = [];
+  if (!this.animating) { return; }
+  if (!this.last) { this.last = timestamp; }
+  const deltaT = (timestamp - this.last) / 1000;
+  this.last = timestamp;
 
-Animation.prototype._step = function(timestamp) {
-  this._animation_request_ids = [];
-  if (!this._animating) { return; }
-  if (!this._last) { this._last = timestamp }
-  var delta_t = (timestamp - this._last) / 1000;
-  this._last = timestamp;
+  this.draw(deltaT);
 
-  this._draw(delta_t);
-  
-  if (this._animating) {
-    this._animation_request_ids.push(
-      this._getNextFrame()
-    );
+  if (this.animating) {
+    this.animation_request_ids.push(this.getNextFrame());
   }
 };
 
