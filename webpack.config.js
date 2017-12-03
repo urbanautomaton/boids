e2df302916b13f0900c50df2edfc3efe17b3a056
Simon Coffey <simon@urbanautomaton.com>
2017-12-03 17:11:50 +0000

    lint blocking compilation is awful

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,6 @@ module.exports = {
         loader: 'babel-loader',
       },
       {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /node_modules|vendor|dist/,
-        loader: 'eslint-loader',
-      },
-      {
         test: require.resolve('./vendor/sylvester'),
         use: 'exports-loader?Matrix,Vector',
       },
