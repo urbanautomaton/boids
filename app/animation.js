f7719b4ec6033a3f55152639ecc8ba67db5e76d6
Simon Coffey <simon@urbanautomaton.com>
2018-01-30 23:34:03 +0000

    ES6 class for Animation

--- app/animation.js
+++ app/animation.js
@@ -1,66 +1,68 @@
-function Animation(doc, win, draw) {
-  this.document = doc;
-  this.window = win;
-  this.animating = false;
-  this.animation_request_ids = [];
-  this.last = null;
-  this.draw = draw;
-  this.frameRate = 0;
-
-  const obj = this;
-
-  // sort this out so it doesn't clobber animating state
-  this.document.addEventListener('visibilitychange', () => {
-    if (obj.document.visibilityState === 'hidden') {
-      obj.pause();
-    } else {
-      obj.play();
-    }
-  });
-}
-
-Animation.prototype.getNextFrame = function getNextFrame() {
-  const obj = this;
-
-  return this.window.requestAnimationFrame((timestamp) => { obj.step(timestamp); });
-};
-
-Animation.prototype.pause = function pause() {
-  if (this.animating) {
+class Animation {
+  constructor(doc, win, draw) {
+    this.document = doc;
+    this.window = win;
     this.animating = false;
-    this.animation_request_ids.forEach((id) => { this.window.cancelAnimationFrame(id); });
     this.animation_request_ids = [];
-  }
-};
-
-Animation.prototype.play = function play() {
-  if (!this.animating) {
-    this.animating = true;
     this.last = null;
-    this.getNextFrame();
+    this.draw = draw;
+    this.frameRate = 0;
+
+    const obj = this;
+
+    // sort this out so it doesn't clobber animating state
+    this.document.addEventListener('visibilitychange', () => {
+      if (obj.document.visibilityState === 'hidden') {
+        obj.pause();
+      } else {
+        obj.play();
+      }
+    });
   }
-};
 
-Animation.prototype.toggle = function toggle() {
-  if (this.animating) {
-    this.pause();
-  } else {
-    this.play();
-  }
-};
+  getNextFrame() {
+    const obj = this;
 
-Animation.prototype.step = function step(timestamp) {
-  this.animation_request_ids = [];
-  if (!this.animating) { return; }
-  if (!this.last) { this.last = timestamp; }
-  const deltaT = (timestamp - this.last) / 1000;
-  this.last = timestamp;
+    return this.window.requestAnimationFrame((timestamp) => { obj.step(timestamp); });
+  };
 
-  this.draw(deltaT);
+  pause() {
+    if (this.animating) {
+      this.animating = false;
+      this.animation_request_ids.forEach((id) => { this.window.cancelAnimationFrame(id); });
+      this.animation_request_ids = [];
+    }
+  };
 
-  if (this.animating) {
-    this.animation_request_ids.push(this.getNextFrame());
-  }
-};
+  play() {
+    if (!this.animating) {
+      this.animating = true;
+      this.last = null;
+      this.getNextFrame();
+    }
+  };
+
+  toggle() {
+    if (this.animating) {
+      this.pause();
+    } else {
+      this.play();
+    }
+  };
+
+  step(timestamp) {
+    this.animation_request_ids = [];
+    if (!this.animating) { return; }
+    if (!this.last) { this.last = timestamp; }
+    const deltaT = (timestamp - this.last) / 1000;
+    this.last = timestamp;
+
+    this.draw(deltaT);
+
+    if (this.animating) {
+      this.animation_request_ids.push(this.getNextFrame());
+    }
+  };
+}
 
 export default Animation;
